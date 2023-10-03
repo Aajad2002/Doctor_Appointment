4303b346dae5e3f0d52276a327bd31ca51d476e2
Aajad2002 <aajadprajapat28@gmail.com>
2023-10-03 11:02:50 +0530

    backend

--- Utils/StaticData.js
+++ Utils/StaticData.js
@@ -1,6 +1,6 @@
 
 function getRandomDoctorImage() {
-    const doctorsImages=[
+    const      doctorsImages=[
         "https://images.pexels.com/photos/4021801/pexels-photo-4021801.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "https://images.pexels.com/photos/4225880/pexels-photo-4225880.jpeg?auto=compress&cs=tinysrgb&w=400",
         "https://images.pexels.com/photos/5726794/pexels-photo-5726794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
